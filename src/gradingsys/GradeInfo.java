b5af3dab5370708c5743dbd4a6d9fc1236550200
ottagit <ottachris13@yahoo.com>
2016-05-24 09:42:25 +0300

    Initial commit

---
+++ src/gradingsys/GradeInfo.java
@@ -0,0 +1,13 @@
+package gradingsys;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+
+public class GradeInfo {
+	
+	public GradeInfo() {
+		// TODO Auto-generated constructor stub
+	}
+}
